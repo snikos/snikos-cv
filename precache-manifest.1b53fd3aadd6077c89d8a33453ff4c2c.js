ab490d04512e123739253933f00841b8b2271e7e
Niko <cobrascript@yandex.ru>
2022-06-12 15:49:04 +0300

    Updates

---
+++ precache-manifest.1b53fd3aadd6077c89d8a33453ff4c2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eed2a6aaf6413939e2fd",
+    "url": "/snikos-cv/static/css/main.eb00cee2.chunk.css"
+  },
+  {
+    "revision": "eed2a6aaf6413939e2fd",
+    "url": "/snikos-cv/static/js/main.844c6db1.chunk.js"
+  },
+  {
+    "revision": "aad20bddcbcd01195231",
+    "url": "/snikos-cv/static/js/runtime~main.f9d4b076.js"
+  },
+  {
+    "revision": "047cb41bbbcdeecc91eb",
+    "url": "/snikos-cv/static/css/2.9cfc23ec.chunk.css"
+  },
+  {
+    "revision": "047cb41bbbcdeecc91eb",
+    "url": "/snikos-cv/static/js/2.79d9b4e2.chunk.js"
+  },
+  {
+    "revision": "be7c1fd009d17cee3bfda94f94ee68da",
+    "url": "/snikos-cv/index.html"
+  }
+];
